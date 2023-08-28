7d9a8e597f4e8ef44b34dfa6327e7765002d02b3
muhammadarslanshahzad <marslanshahzad14@gmail.com>
2023-08-28 09:55:15 +0500

    finalizing app

---
+++ main.py
@@ -0,0 +1,69 @@
+import sys
+sys.path.append('/mnt/e/cv_mlops/src')  # Add the path to the 'src' folder
+
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline 
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
+
+
+###
+# Data Ingestion Stage Pipeline
+####
+STAGE_NAME = 'Data Ingestion stage'
+try:
+    logger.info(f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n\n xxxxxxxxxxxx===================================================================================================================xxxxxxxxxxxxxx')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+###
+#  Preparing Base Model Pipeline 
+###
+STAGE_NAME = 'Prepare Base Model'
+try:
+    logger.info(f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n\n xxxxxxxxxxxx===================================================================================================================xxxxxxxxxxxxxx')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise 
+
+
+###
+# Model Evaluation
+##
+
+
+STAGE_NAME = "Evaluation"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = EvaluationPipeline()
+   model_evaluation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise 
+
---
+++ src/cnnClassifier/pipeline/stage_04_evaluation.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append('/mnt/e/cv_mlops/src')  # Add the path to the 'src' folder
+
+
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.evaluation import Evaluation
+from cnnClassifier import logger
+
+
+STAGE_NAME = "Evaluation stage"
+
+class EvaluationPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        val_config = config.get_validation_config()
+        evaluation = Evaluation(val_config)
+        evaluation.evaluation()
+        evaluation.save_score()
+
+
+
+if __name__ == '__main__':
+    
+    try:
+        logger.info(f"==============================================************************************************************************************=====================================================================")
+        logger.info(f"=============================>>>>>>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<=========================================================================")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f"==================================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<<<<<<<===================================================================")
+    
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
